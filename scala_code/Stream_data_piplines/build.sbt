05a1c1b5a30d0c066a340b2d51ab42a3a9fc6886
Amit Singh <amit8singh008@gmail.com>
2024-06-23 17:48:30 +0530

    update sbt plugins for fat Jar where discard is default strategy now

--- scala_code/Stream_data_piplines/build.sbt
+++ scala_code/Stream_data_piplines/build.sbt
@@ -9,5 +9,6 @@ lazy val root = (project in file("."))
       "org.apache.spark" %% "spark-core" % "3.5.1" % "provided",
       "org.apache.spark" %% "spark-sql" % "3.5.1" % "provided",
       "org.scala-lang" % "scala-reflect" % "2.13.14"
-    )
+    ),
+    assembly / assemblyJarName := "spark-streamingdata-pipeline-1.0.jar",
   )
--- scala_code/Stream_data_piplines/src/main/scala/TestExample.scala
+++ scala_code/Stream_data_piplines/src/main/scala/TestExample.scala
@@ -1,27 +1,25 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, max}
 
-  import org.apache.spark.sql.SparkSession
-  import org.apache.spark.sql.functions.{col, max}
+object TestExample extends App {
 
-  object TestExample extends App {
+  val spark = SparkSession
+    .builder
+    .appName("Example 1 to check spark is running fine")
+    .config("spark.sql.shuffle.partitions", "5")
+    .master("local[*]")
+    .getOrCreate()
 
-    val spark = SparkSession
-      .builder
-      .appName("Example 1 to check spark is running fine")
-      .config("spark.sql.shuffle.partitions", "5")
-      .master("local[*]")
-      .getOrCreate()
+  val flightData2015 = spark
+    .read
+    .option("inferSchema", "true")
+    .option("header", "true")
+    .csv("C:/Users/Public/Documents/Spark/Spark-The-Definitive-Guide/data/flight-data/csv/2015-summary.csv")
 
-    val flightData2015 = spark
-      .read
-      .option("inferSchema", "true")
-      .option("header", "true")
-      .csv("C:/Users/Public/Documents/Spark/Spark-The-Definitive-Guide/data/flight-data/csv/2015-summary.csv")
-
-    flightData2015.select(max(col("count"))).show()
-    flightData2015.show(5)
+  flightData2015.select(max(col("count"))).show()
+  flightData2015.show(5)
 
 
-    //Hold the run for 5 mins to check Web UI
-    // Thread.sleep(300000)
+  //Hold the run for 5 mins to check Web UI
+  // Thread.sleep(300000)
 }
