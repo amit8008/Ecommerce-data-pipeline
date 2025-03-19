6f0bbbcebba8e4aef1db2ae2762584c49580c1b4
Amit Singh <amit8singh008@gmail.com>
2025-03-19 22:07:01 +0530

    implement pandas dataframe in data simulator

--- python_code/streamDataPipline/data_simulator/seller_data_simulator.py
+++ python_code/streamDataPipline/data_simulator/seller_data_simulator.py
@@ -1,8 +1,11 @@
 import random
 import json
 from faker import Faker
+import pandas as pd
 
 fake = Faker()
+
+resource_loc = "C:\\Users\\Public\\Documents\\Stream-data-pipelines\\python_code\\streamDataPipline\\resources\\"
 
 
 def generate_fake_seller(output_type: str = "raw") :
@@ -28,14 +31,24 @@ def generate_fake_seller(output_type: str = "raw") :
 num_products = 3
 fake_seller = [generate_fake_seller("dict") for _ in range(num_products)]
 
-# Save to JSON file
-with open(
-        "C:\\Users\\Public\\Documents\\Stream-data-pipelines\\python_code\\streamDataPipline\\resources\\fake_seller1.json",
-        "w") as f :
-    json.dump(fake_seller, f, indent = 4)
+# Save to JSON file with pandas
+df = pd.DataFrame(data = fake_seller)
+
+df.to_json(resource_loc + "fake_seller2.json", orient = "records", indent = 4)
 
 # Print a sample product
 print(json.dumps(fake_seller[:2], indent = 4))
 
 fake_seller1 = generate_fake_seller()
 print(fake_seller1)
+
+# a = []
+# d = {'col1':1, 'col2':4}
+# a.append(d)
+# d1 = {'col1':2, 'col2':3}
+# a.append(d1)
+
+# df = pd.DataFrame(data = a)
+# df = pd.DataFrame(data = fake_seller)
+
+# print(df)
