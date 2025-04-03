6f3c7dcbb63aa33a78fb8b75e1450d492ee69019
Amit Singh <amit8singh008@gmail.com>
2025-04-03 23:52:34 +0530

    updated customer generator to dump data into postgresql

--- python_code/streamDataPipline/src/data_simulator/customer_data_simulator.py
+++ python_code/streamDataPipline/src/data_simulator/customer_data_simulator.py
@@ -15,7 +15,7 @@ def generate_fake_customer(output_type: str = "raw", delimiter: str = "|") :
     customer_phone = fake.phone_number()
     customer_email = fake.email()
     customer_dob = fake.date_of_birth(minimum_age = 10, maximum_age = 80).isoformat()
-    customer_address = fake.address()
+    customer_address = fake.address().replace("\n", ",")
 
     if output_type == "json" :
         return {
@@ -48,3 +48,29 @@ fake_customer_tuple = [generate_fake_customer("tuple") for _ in range(num_custom
 df_tuple = pd.DataFrame(data = fake_customer_tuple)
 logger.info(f"\n{df_tuple}")
 
+
+# Creating 7 customer for testing with postgresql, generate 7 customer data created as tuple with delimiter |
+from sqlalchemy import create_engine
+import psycopg2
+
+# 1. Define PostgreSQL connection details
+DB_USER = "myuser"
+DB_PASSWORD = "mypassword"
+DB_HOST = "localhost"   # If using Docker, replace with container name e.g., "postgres_db"
+DB_PORT = "5432"
+DB_NAME = "ecommerce_db"
+
+# 2. Create a connection engine
+engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+# 3. Create a sample DataFrame
+customer_psql = [generate_fake_customer("tuple") for _ in range(7)]
+customer_col = ["customer_id", "customer_name", "customer_phone", "customer_email", "customer_dob", "customer_address"]
+df_customer_psql = pd.DataFrame(data = customer_psql, columns = customer_col)
+
+# 4a. save DataFrame in csv file
+df_customer_psql.to_csv(configuration.data_dir + "customer_data_7.tsv",sep = "|", index = False)
+# 4b. Load DataFrame into PostgreSQL
+df_customer_psql.to_sql("customer", engine, if_exists="replace", index=False)
+
+print("Customer Data successfully loaded into PostgreSQL!")
