6d4a4776c07f2eb468c0d3c3a2a2d82344f5ee25
Amit Singh <amit8singh008@gmail.com>
2025-06-08 17:38:27 +0530

    order data streaming set up completed

--- scala_code/Stream_data_piplines/src/main/scala/com/ecommerce/streaming/OrderStream.scala
+++ scala_code/Stream_data_piplines/src/main/scala/com/ecommerce/streaming/OrderStream.scala
@@ -1,7 +1,9 @@
 package com.ecommerce.streaming
 
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.{array_contains, col, split}
+import org.apache.spark.storage.StorageLevel
 
 object OrderStream extends App {
   val spark = SparkSession
@@ -13,9 +15,10 @@ object OrderStream extends App {
   val df = spark
     .readStream
     .format("kafka")
-    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("kafka.bootstrap.servers", "host.docker.internal:9092")
     .option("subscribe", "Ecommerce-order_events")
     .option("startingOffsets", "earliest")
+    .option("spark.sql.streaming.forceDeleteTempCheckpointLocation", "true")
     .load()
 
   import spark.implicits._
@@ -25,15 +28,39 @@ object OrderStream extends App {
     .withColumn("value_str", col("value").cast("String"))
     .drop("key", "value")
 
+  val orderDf = kafkaData
+    .withColumn("order_id", split(col("value_str"), "\\|").getItem(0))
+    .withColumn("prod_id", split(col("value_str"), "\\|").getItem(1))
+    .withColumn("cust_id", split(col("value_str"), "\\|").getItem(2))
+    .withColumn("order_time", split(col("value_str"), "\\|").getItem(3))
+    .select("order_id", "prod_id", "cust_id", "order_time")
+
 //  val kafkaData = df.selectExpr("key", "value") --- give redundant values
 
-//  val orderStream = kafk
+//  Count of product_id in each batch
+  val productCount = orderDf
+    .groupBy(col("prod_id"))
+    .count()
+    .withColumnRenamed("count", "prod_count")
 
+//  join product count df with product df to get other attributes of product
+  val config = ConfigFactory.load()
+
+  val productDf = spark.read
+    .option("multiline", "true")
+    .json(config.getString("Ecommerce.product"))
+    .persist(StorageLevel.MEMORY_AND_DISK)
+
+  val joinedDf = productCount
+    .join(productDf, productDf("product_id") === productCount("prod_id"), "left")
+    .select("prod_id","prod_count", "product_name", "tags")
+    .filter(array_contains(col("tags"), "Premium"))
+    .orderBy("prod_count")
 
   // Print the data to the console
-  val query = kafkaData.writeStream
-    .outputMode("append")
-    //    .outputMode("complete") // only use when streaming aggregation is apply on dataframe
+  val query = joinedDf.writeStream
+//    .outputMode("append")
+        .outputMode("complete") // only use when streaming aggregation is apply on dataframe
     .format("console")
     .option("truncate", "false")
     .start()
