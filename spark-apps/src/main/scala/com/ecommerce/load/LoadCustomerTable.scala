dbe25be58f76b5f4f9df239889e11f1062238570
Amit Singh <amit8singh008@gmail.com>
2025-07-05 20:39:33 +0530

    Separated Iceberg-Spark connection config

--- spark-apps/src/main/scala/com/ecommerce/load/LoadCustomerTable.scala
+++ spark-apps/src/main/scala/com/ecommerce/load/LoadCustomerTable.scala
@@ -1,5 +1,6 @@
 package com.ecommerce.load
 
+import com.ecommerce.utility.IcebergSparkConfig
 import com.typesafe.config.ConfigFactory
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, current_date, lit}
@@ -8,19 +9,13 @@ import java.io.File
 
 object LoadCustomerTable extends App {
   val config = ConfigFactory.parseFile(new File(args(0))).resolve()
+  val icebergConf = IcebergSparkConfig(config)
 
   val spark = SparkSession
     .builder()
     .appName("Load Customer postgresSQL table into Iceberg table")
     .master("local[*]")
-    .config("spark.jars.packages", "org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.9.1")
-    .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-    .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
-    .config("spark.sql.catalog.spark_catalog.type ", "hive")
-    .config("spark.sql.catalog.local", "org.apache.iceberg.spark.SparkCatalog")
-    .config("spark.sql.catalog.local.type", "hadoop")
-    .config("spark.sql.catalog.local.warehouse", config.getString("Iceberg.warehouse"))
-    .config("spark.sql.defaultCatalog", "local")
+    .config(icebergConf)
     .getOrCreate()
 
 
