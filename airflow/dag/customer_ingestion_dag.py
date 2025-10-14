4ab811b7eaf0b3e758bb1fcaddf82085b81a9f95
Amit Singh <amit8singh008@gmail.com>
2025-10-14 15:18:39 +0530

    set up airflow dags for customer tables

--- airflow/dag/customer_ingestion_dag.py
+++ airflow/dag/customer_ingestion_dag.py
@@ -2,18 +2,18 @@ from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime
 
-def ingest_customer_data():
+
+def ingest_customer_data() :
     print("Extracting and loading customer data into bronze Iceberg table.")
 
 
 with DAG(
-    dag_id='customer_ingestion_dag',
-    start_date=datetime(2025, 10, 1),
-    schedule_interval='@daily',
-    catchup=False,
-    tags=['bronze', 'customer']
-) as dag:
-
+        dag_id = 'customer_ingestion_dag',
+        start_date = datetime(2025, 10, 1),
+        schedule_interval = '@daily',
+        catchup = False,
+        tags = ['bronze', 'customer']
+) as dag :
     extract_to_bronze = SparkSubmitOperator(
         task_id = 'extract_customer_to_bronze',
         application = '/ecom/libs/Stream_data_pipelines-assembly-0.1.0-SNAPSHOT.jar',
